4c132eea02ac90ec8db2bded25a8b4721caab71e
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-10-01 09:27:58 -0400

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+])
